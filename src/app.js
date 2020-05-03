74d348f438d8035b62f31a8c9981e3202a0538fb
Helton Silva <helton.system@hotmail.com>
2020-05-03 20:48:01 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  // TODO TESTE
 });
 
 app.post("/repositories", (request, response) => {
